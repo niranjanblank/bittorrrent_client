1a3b2b578abffd2c2d7b86c933e98440ddeb00ed
Niranjan Shah <niranjanshah474@gmail.com>
2024-10-19 21:05:50 +1100

    simple bencoded_decoder wrote that just finds : and gets the digit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
+#include<string>
+#include "lib/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+void decode_bencoded_value(const std::string& encoded_value){
+  // string benced value have this format digit: string , here digit = length of string
+  if(std::isdigit(encoded_value[0])){
+    size_t colon_index = encoded_value.find(":");
+    std::cout <<"First Occurence at: " << colon_index << std::endl;;
+    std::cout << json(encoded_value.substr(0,colon_index));
+  }
+}
 
 int main(int argc, char* argv[]){
-  std::cout<<"Hello World";
+  
+  
+  decode_bencoded_value("5:hello");
   return 0; 
 }
