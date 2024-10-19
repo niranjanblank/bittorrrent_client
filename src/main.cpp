a49c317874db6e9ed8e25bf79dab20da9a49da2f
Niranjan Shah <niranjanshah474@gmail.com>
2024-10-19 21:57:09 +1100

    string decoded

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,11 @@ void decode_bencoded_value(const std::string& encoded_value){
   // string benced value have this format digit: string , here digit = length of string
   if(std::isdigit(encoded_value[0])){
     size_t colon_index = encoded_value.find(":");
-    std::cout <<"First Occurence at: " << colon_index << std::endl;;
-    std::cout << json(encoded_value.substr(0,colon_index));
+    std::cout <<"First Occurence at: " << colon_index << std::endl;
+    int length_of_string = std::stoi(encoded_value.substr(0,colon_index));
+    
+    std::cout << length_of_string << std::endl;
+    std::cout << encoded_value.substr(colon_index+1,  length_of_string);
   }
 }
 
