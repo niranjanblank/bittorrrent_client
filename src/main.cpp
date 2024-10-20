525d3de865dd5628bc40c039721d463833e921ea
Niranjan Shah <niranjanshah474@gmail.com>
2024-10-20 23:47:55 +1100

    minor changes to decode_bencoded_value function to return a json

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,20 @@
 
 using json = nlohmann::json;
 
-void decode_bencoded_value(const std::string& encoded_value){
+json decode_bencoded_value(const std::string& encoded_value){
   // string benced value have this format digit: string , here digit = length of string
   if(std::isdigit(encoded_value[0])){
     size_t colon_index = encoded_value.find(":");
-    std::cout <<"First Occurence at: " << colon_index << std::endl;
     int length_of_string = std::stoi(encoded_value.substr(0,colon_index));
     
-    std::cout << length_of_string << std::endl;
-    std::cout << encoded_value.substr(colon_index+1,  length_of_string);
+    return json(encoded_value.substr(colon_index+1,  length_of_string));
+
   }
 }
 
 int main(int argc, char* argv[]){
   
-  
-  decode_bencoded_value("5:hello");
+  json decoded_value = decode_bencoded_value("5:hello");
+  std::cout << decoded_value;
   return 0; 
 }
