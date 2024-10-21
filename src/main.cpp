b1d338370f3d68b76975aecd3ebe546c666c472f
Niranjan Shah <niranjanshah474@gmail.com>
2024-10-21 20:20:44 +1100

    integer decoding added

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 using json = nlohmann::json;
 
 json decode_bencoded_value(const std::string& encoded_value){
-  // string benced value have this format digit: string , here digit = length of string
+  // string bencoded value have this format digit: string , here digit = length of string
   if(std::isdigit(encoded_value[0])){
     size_t colon_index = encoded_value.find(":");
     int length_of_string = std::stoi(encoded_value.substr(0,colon_index));
@@ -13,11 +13,34 @@ json decode_bencoded_value(const std::string& encoded_value){
     return json(encoded_value.substr(colon_index+1,  length_of_string));
 
   }
+  else if(encoded_value[0]=='i' && encoded_value.back()=='e'){
+    //extracting integer part
+    std::string int_part = encoded_value.substr(1,encoded_value.size()-2);
+    if( (int_part[0]=='0' && int_part !="0") || (int_part[0]=='-' && int_part[1]=='0')){
+      // invalid integer encoding
+      throw std::runtime_error("Invalid Integer Encoding");
+    }
+    else{
+      // parse the string to get integer
+      return json(std::stoll(int_part));
+    }
+    
+  }
+  else {
+    throw std::runtime_error("Unhandled encoded value: "+encoded_value);
+  }
 }
 
 int main(int argc, char* argv[]){
-  
-  json decoded_value = decode_bencoded_value("5:hello");
-  std::cout << decoded_value;
+  if(argc>1){
+    try{
+      std::string input_encoded_value = argv[1];
+      json decoded_value = decode_bencoded_value(input_encoded_value);
+      std::cout << decoded_value;
+    }
+    catch(const std::exception& e){
+      std::cerr << "Error:" << e.what() <<std::endl;
+    }
+  }
   return 0; 
 }
