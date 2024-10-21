9ef913fb51e06db7216c550c606fb0e0ca810a81
Niranjan Shah <niranjanshah474@gmail.com>
2024-10-21 21:18:40 +1100

    validation added for string encoding

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,23 @@ using json = nlohmann::json;
 json decode_bencoded_value(const std::string& encoded_value){
   // string bencoded value have this format digit: string , here digit = length of string
   if(std::isdigit(encoded_value[0])){
-    size_t colon_index = encoded_value.find(":");
-    int length_of_string = std::stoi(encoded_value.substr(0,colon_index));
-    
-    return json(encoded_value.substr(colon_index+1,  length_of_string));
-
-  }
+        size_t colon_index = encoded_value.find(":");
+        if(colon_index != std::string::npos){
+          
+          int length_of_string = std::stoi(encoded_value.substr(0,colon_index));
+          // extracted string from colon_index+1 to the end 
+          std::string extracted_string = encoded_value.substr(colon_index+1);
+          // validate length of extracted string matches actual length 
+          if(extracted_string.size() < static_cast<size_t>(length_of_string)){
+            throw std::runtime_error("Invalid encoded value: length mismatch for "+encoded_value);
+          }
+          return json(extracted_string.substr(0,  length_of_string));
+        }
+        else{
+          // if no : is found
+          throw std::runtime_error("Invalid encoded value:"+encoded_value);
+        }
+     }
   else if(encoded_value[0]=='i' && encoded_value.back()=='e'){
     //extracting integer part
     std::string int_part = encoded_value.substr(1,encoded_value.size()-2);
